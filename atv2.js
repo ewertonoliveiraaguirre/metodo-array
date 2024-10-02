7952186eb5ffd627ba6878557717eb9fbaa16970
ewertonoliveiraaguirre <126290541+ewertonoliveiraaguirre@users.noreply.github.com>
2024-10-02 17:09:49 -0400

    Add files via upload

---
+++ atv2.js
@@ -0,0 +1,14 @@
+let carrinhoCompra = ['batata','arroz','açucar','feijão'];
+console.log(carrinhoCompra);
+
+//adicionando  produtos ao carrinho
+carrinhoCompra.push('carne','óleo')
+console.log(carrinhoCompra);
+
+//removendo o primeiro item da lista (batata)
+carrinhoCompra.shift()
+console.log(carrinhoCompra);
+
+//colocando os elementos do carrihop em ordem alfabética
+carrinhoCompra.sort()
+console.log(carrinhoCompra);
